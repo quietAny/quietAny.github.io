cbe137ac2b1772451db6a944262fd28f123a0b01
guohao <353225790@qq.com>
2019-08-29 11:30:51 +0800

    Site updated: 2019-08-29 11:30:49

--- live2d-widget/autoload.js
+++ live2d-widget/autoload.js
@@ -1,3 +1,3 @@
-// build time:Mon Aug 26 2019 10:41:23 GMT+0800 (GMT+08:00)
+// build time:Thu Aug 29 2019 11:30:43 GMT+0800 (GMT+08:00)
 const live2d_path="/live2d-widget/";$("<link>").attr({href:live2d_path+"waifu.css",rel:"stylesheet"}).appendTo("head");$.ajax({url:live2d_path+"live2d.min.js",dataType:"script",cache:true});$.ajax({url:live2d_path+"waifu-tips.js",dataType:"script",cache:true});$(window).on("load",function(){initWidget(live2d_path+"waifu-tips.json","https://live2d.fghrsh.net/api")});
 //rebuild by neat 
